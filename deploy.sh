7e5d331c9411f948a46dee3d44b094bebb7df60d
Aleksi Martikainen <martta_1994@hotmail.com>
2023-01-25 19:33:37 +0200

    Optimize deployment process to minimize down time

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker kill highscores-backend
-docker rm highscores-backend
 git pull
 docker build -t highscores-backend .
+docker kill highscores-backend
+docker rm highscores-backend
 docker run -d -p 80:8080 --name highscores-backend --restart=always highscores-backend
