7eb33648f3593d67d76e8c46b440615b0b5f7873
Aleksi Martikainen <martta_1994@hotmail.com>
2023-02-02 13:57:39 +0200

    Add 80 port

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git pull
 docker build -t highscores-backend .
 docker kill highscores-backend
 docker rm highscores-backend
-docker run -d -p 443:443 --name highscores-backend --restart=always highscores-backend
+docker run -d -p 443:443 -p 80:80 --name highscores-backend --restart=always highscores-backend
