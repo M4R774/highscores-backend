fcc750d191716b2c5b7cc24d82314c1ef7325728
Martta <martta_1994@hotmail.com>
2023-03-21 16:39:32 +0200

    Add docker script

--- swagger-ui/run-docker.sh
+++ swagger-ui/run-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 8888:8080 -e SWAGGER_JSON=/openapi-definition.yaml -v ./swagger-ui/openapi-definition.yaml:/openapi-definition.yaml pentusha/swagger-ui-crossbuild:latest
+docker run --restart always -p 8888:8080 -e SWAGGER_JSON=/openapi-definition.yaml -v ./swagger-ui/openapi-definition.yaml:/openapi-definition.yaml pentusha/swagger-ui-crossbuild:latest
