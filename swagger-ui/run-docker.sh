5275e5daa95baf266a22dff4fda7b7fe44beb123
Aleksi Martikainen <martta_1994@hotmail.com>
2023-03-21 16:45:43 +0200

    Fix name parameter

--- swagger-ui/run-docker.sh
+++ swagger-ui/run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker kill swagger
-docker run -d -n swagger --restart always -p 8888:8080 -e SWAGGER_JSON=/openapi-definition.yaml -v ./swagger-ui/openapi-definition.yaml:/openapi-definition.yaml pentusha/swagger-ui-crossbuild:latest
+docker run -d --name swagger --restart always -p 8888:8080 -e SWAGGER_JSON=/openapi-definition.yaml -v ./swagger-ui/openapi-definition.yaml:/openapi-definition.yaml pentusha/swagger-ui-crossbuild:latest
